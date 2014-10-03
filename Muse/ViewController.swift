ef7e5655143eb7c65661d60b29dee70f68beb402
Yuk Youngho <six605@gmail.com>
2014-10-03 18:10:11 +0900

    Add Muse Xcode project files

---
+++ Muse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muse
+//
+//  Created by Yuk Youngho on 2014. 10. 3..
+//  Copyright (c) 2014년 Genosis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
