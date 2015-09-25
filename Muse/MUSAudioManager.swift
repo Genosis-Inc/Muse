7c5bcb4a61c0f0203ac98d1bb5f62a3e00a857ba
Youngho Yuk <six605@gmail.com>
2015-09-26 00:46:59 +0900

    1. 음악 재생 상태에 따라 Play 버튼의 텍스트 변경 음악이 재생 중일 경우 : "Pause" 음악이 일시 중지일 경우 : "Play"
    2. 음악 재생 상태에 따라 Play 버튼의 동작 변경
    음악이 재생 중일 경우 클릭 : 음악 일시 정지
    음악이 일시 중지일 경우 클릭 : 음악 재생
    
    MUS-89 MUS-97 #resolve

--- Muse/MUSAudioManager.swift
+++ Muse/MUSAudioManager.swift
@@ -18,36 +18,44 @@ class MUSAudioManager
     
     init()
     {
+        // 1. SystemMusicPlayer 인스턴스 생성
         sysMusicPlayer = MPMusicPlayerController.systemMusicPlayer()
-        if (nil != sysMusicPlayer)
-        {
-            sysMusicPlayer.beginGeneratingPlaybackNotifications()
-            sysMusicPlayer.setQueueWithItemCollection(MPMediaItemCollection(items: MPMediaQuery.songsQuery().items!))
-        }
+        
+        // 2. Notification 등록
+        sysMusicPlayer.beginGeneratingPlaybackNotifications()
+        
+        // 3. Playback 설정
+        sysMusicPlayer.setQueueWithItemCollection(MPMediaItemCollection(items: MPMediaQuery.songsQuery().items!))
     }
     
     deinit
     {
-        
+        sysMusicPlayer.endGeneratingPlaybackNotifications()
     }
     
     /// 현재 Playback을 재생 합니다.
     func play()
     {
-        if (nil != sysMusicPlayer)
-        {
-            sysMusicPlayer.play()
-        }
+        sysMusicPlayer.play()
+    }
+    
+    /// 재생을 일시중지 한다.
+    func pause()
+    {
+        sysMusicPlayer.pause()
+    }
+    
+    /// 재생을 멈춘다.
+    func stop()
+    {
+        sysMusicPlayer.stop()
     }
     
     /// 현재 재생중인 음악에서 다음 음악을 재생 합니다.
     func playNext()
     {
-        if (nil != sysMusicPlayer)
-        {
-            sysMusicPlayer.skipToNextItem()
-            sysMusicPlayer.play()
-        }
+        sysMusicPlayer.skipToNextItem()
+        sysMusicPlayer.play()
     }
     
     /// 현재 재생중인 음악의 이전 음악을 재생합니다.
@@ -55,5 +63,11 @@ class MUSAudioManager
     {
         sysMusicPlayer.skipToPreviousItem()
         sysMusicPlayer.play()
+    }
+    
+    /// 현재 Playback state 리턴한다.
+    func getPlaybackState() -> MPMusicPlaybackState
+    {
+        return sysMusicPlayer.playbackState
     }
 }
--- Muse/ViewController.swift
+++ Muse/ViewController.swift
@@ -12,40 +12,96 @@ import MediaPlayer
 
 class ViewController: UIViewController {
     
-    let MUSAudioMgr : MUSAudioManager = MUSAudioManager()   // 음악 관리자
-    @IBOutlet weak var artWorkView: UIImageView!
+    let MUSAudioMgr : MUSAudioManager = MUSAudioManager()
     
+    @IBOutlet weak var artWorkView: UIImageView!
+    @IBOutlet var btnPlayStop: UIButton!
+    
+    /// Playback 현재 음악 재생
     @IBAction func play(sender: UIButton)
     {
-        MUSAudioMgr.play()
+        let playbackState: MPMusicPlaybackState = MUSAudioMgr.getPlaybackState()
+        
+        switch (playbackState)
+        {
+        case MPMusicPlaybackState.Paused:
+            MUSAudioMgr.play()
+            break
+        case MPMusicPlaybackState.Playing:
+            MUSAudioMgr.pause()
+            break
+        case MPMusicPlaybackState.Stopped:
+            MUSAudioMgr.play()
+            break
+        default:
+            break
+        }
     }
 
+    /// Playback 이전 음악 재생
     @IBAction func PlayPrev(sender: AnyObject)
     {
         MUSAudioMgr.playPrev()
     }
     
+    /// Playback 다음 음악 재생
     @IBAction func PlayNext(sender: AnyObject)
     {
         MUSAudioMgr.playNext()
     }
     
+    
+    /// 재생중인 음악이 변경되었을 경우 처리
+    ///
+    /// MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+    /// 재생 변경된 음악의 앨범을 출력한다.
     func NowPlayingItemDidChanged(notification: NSNotification)
     {
         artWorkView.image = MUSAudioMgr.sysMusicPlayer.nowPlayingItem!.artwork!.imageWithSize(artWorkView.intrinsicContentSize())
     }
     
-    override func viewDidLoad() {
+    /// 재생 상태가 변경되었을 경우 처리
+    ///
+    /// MPMusicPlayerControllerPlaybackStateDidChangeNotification 이벤트 핸들러
+    func PlaybakStateChanged(notification: NSNotification)
+    {
+        let playbackState: MPMusicPlaybackState = MUSAudioMgr.getPlaybackState()
+        
+        switch (playbackState)
+        {
+        case MPMusicPlaybackState.Paused:
+            btnPlayStop.setTitle("Play", forState: .Normal)
+            break
+        case MPMusicPlaybackState.Playing:
+            btnPlayStop.setTitle("Pause", forState: .Normal)
+            break
+        case MPMusicPlaybackState.Stopped:
+            btnPlayStop.setTitle("Play", forState: .Normal)
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         NSNotificationCenter.defaultCenter().addObserver(self,
-            selector: "NowPlayingItemDidChanged:",
-            name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification,
-            object: nil)
+                                                         selector: "NowPlayingItemDidChanged:",
+                                                         name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification,
+                                                         object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: "PlaybakStateChanged:",
+                                                         name: MPMusicPlayerControllerPlaybackStateDidChangeNotification,
+                                                         object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
